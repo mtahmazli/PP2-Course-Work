832ee1aef888021518dc60bd9fbbcaecaaa37935
mtahmazli <mrdtehmezli@gmail.com>
2023-02-02 11:49:14 +0400

    First Commit

---
+++ Second.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Second {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        long a=scanner.nextLong();
+        long b=scanner.nextLong();
+        long c=scanner.nextLong();
+        long sum;
+        sum=a+b+c;
+        System.out.println("Long: "+sum);
+        System.out.println("*******************");
+        float a1=scanner.nextFloat();
+        float b1=scanner.nextFloat();
+        float c1=scanner.nextFloat();
+        float sum1;
+        sum1=a1+b1+c1;
+        System.out.println("Float: "+sum1);
+
+    }
+}
